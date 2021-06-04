aa550849bf71524f5454fd6a83d6f49551b5c0f3
Tyler Weston <tylerjohnweston@gmail.com>
2021-06-04 15:16:18 -0400

    remove grid lines in outer wall

--- sketch.js
+++ sketch.js
@@ -14,8 +14,11 @@ menu is opened? Something with unclick happening!
   - make button class? or checkbox class?
 - Detect if the user is on mobile and make the grid smaller so it
   is easier to choose/move lights, etc.
+    - This now requires a few more fixes! Work on this, ie.
+      mobile mode vs PC mode
 
 Visual fixes:
+- decrease the offset around checking for shadow edges?
 - with new auto-sizing, the game doesn't end up lined up in the middle,
   EITHER use an offset OR just round to neareast multiple of 20?
 - Main menu should be better lined up... looks OK with new font
@@ -259,7 +262,7 @@ class undo
 // data classes, mostly holding vars, enums, etc.
 class game
 {
-// make the playing field 20x20
+  // make the playing field a different size depending if we're on mobile
   static PLAYFIELD_DIM;
   static PC_PLAYFIELD_DIM = 22;
   static MOBILE_PLAYFIELD_DIM = 14;
@@ -1906,7 +1909,7 @@ function windowResized()
 
 function initialize_colors() {
   palette.solid_wall_fill = color(160, 160, 170);
-  palette.solid_wall_permenant_fill = color(200, 200, 210);
+  palette.solid_wall_permenant_fill = color(180, 180, 190);
   palette.solid_wall_outline = color(120, 120, 120);
 
   palette.buildable_fill = color(33, 33, 33);
@@ -2880,7 +2883,10 @@ function draw_walls_and_floors()
       {
         if (lvl.grid[x][y].fade < 1)
           lvl.grid[x][y].fade += 0.1;
-        stroke(palette.solid_wall_outline);
+        if (p)
+          noStroke();
+        else
+          stroke(palette.solid_wall_outline);
         // exact same thing as above!
         fill(lerpColor( odd ? palette.buildable_fill : palette.buildable_2_fill, 
                         p ? palette.solid_wall_permenant_fill : palette.solid_wall_fill, 
@@ -2894,6 +2900,7 @@ function draw_walls_and_floors()
         stroke(170, 170, 170);
         if (lvl.grid[x][y].permenant)
         {
+          noStroke();
           fill(170, 170, 170, 40);
         }
         square(x * game.gridSize + 1, y * game.gridSize + 1, game.gridSize - 3);
@@ -4188,6 +4195,7 @@ document.addEventListener('keydown', function(event) {
   }
 });
 
+// from https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 window.mobileCheck = function() {
   let check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
